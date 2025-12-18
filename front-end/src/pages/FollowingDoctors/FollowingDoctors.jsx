2709ba358066274e15589df3064071c3eeb9ddd7
PradeepDev07 <pradeep.m.developer@gmail.com>
2025-12-18 08:59:33 +0530

    bug: changed the link to navigation

--- front-end/src/pages/FollowingDoctors/FollowingDoctors.jsx
+++ front-end/src/pages/FollowingDoctors/FollowingDoctors.jsx
@@ -1,10 +1,8 @@
 import Navbar from '../../components/Navbar/Navbar'
 import { useContext } from 'react'
-<<<<<<< HEAD
+
 import {  useNavigate} from 'react-router-dom'
-=======
-import { useNavigate } from 'react-router-dom'
->>>>>>> 758c11c5f4987141ddd57349a95138c9e5c4b3f3
+
 import { StorageContext } from '../../context/StorageContext'
 import styles from './FollowingDoctors.module.css'
 
@@ -12,10 +10,10 @@ import styles from './FollowingDoctors.module.css'
 const FollowingDoctors = () => {
   const { followingDoctors, unfollowDoctor } = useContext(StorageContext)
   const navigate = useNavigate()
-<<<<<<< HEAD
 
-=======
->>>>>>> 758c11c5f4987141ddd57349a95138c9e5c4b3f3
+
+
+
 
   return (
     <>
@@ -28,14 +26,10 @@ const FollowingDoctors = () => {
         {followingDoctors && followingDoctors.length > 0 ? (
           <ul className={styles.list}>
             {followingDoctors.map((doc) => (
-<<<<<<< HEAD
-              <li key={doc.id} className={styles.item} >
-              
+
+              <li key={doc.id} className={styles.item}>
                 <div className={styles.info} style={{cursor: 'pointer'}} onClick={() => navigate(`/doctors/${doc.id}`)}>
-=======
-              <li key={doc.id} className={styles.item} onClick={()=>navigate("/doctors/{id}".replace("{id}",doc.id))}>
-                <div className={styles.info}>
->>>>>>> 758c11c5f4987141ddd57349a95138c9e5c4b3f3
+
                   {doc.image && <img src={doc.image} alt={doc.name} className={styles.avatar} />}
                   <div>
                     <div className={styles.name}>{doc.name}</div>
@@ -53,6 +47,7 @@ const FollowingDoctors = () => {
           <p className={styles.empty}>You are not following any doctors yet.</p>
         )}
       </div>
+
     </div>
     </>
   )
